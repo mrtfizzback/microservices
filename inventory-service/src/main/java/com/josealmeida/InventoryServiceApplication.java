d3e29bb1d815f71e5c2d348624ef9026596e9beb
jose_Almeida <josepedrosmith@msn.com>
2024-02-06 00:07:51 +0000

    Initial commit

---
+++ inventory-service/src/main/java/com/josealmeida/InventoryServiceApplication.java
@@ -0,0 +1,32 @@
+package com.josealmeida;
+
+import com.josealmeida.model.Inventory;
+import com.josealmeida.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+        return args -> {
+            Inventory inventory = new Inventory();
+            inventory.setSkuCode("Apple Vision pro");
+            inventory.setQuantity(100);
+
+            Inventory inventory1 = new Inventory();
+            inventory1.setSkuCode("Apple Vision pro 2");
+            inventory1.setQuantity(0);
+
+            inventoryRepository.save(inventory);
+            inventoryRepository.save(inventory1);
+        };
+    }
+}
---
+++ order-service/src/main/java/com/josealmeida/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.josealmeida.repository;
+
+import com.josealmeida.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
