a9d502ab6b44db3023a99d35b330bc9eab0e1aa4
jose_Almeida <josepedrosmith@msn.com>
2024-02-08 00:27:55 +0000

    service discovery working

--- inventory-service/src/main/java/com/josealmeida/service/InventoryService.java
+++ inventory-service/src/main/java/com/josealmeida/service/InventoryService.java
@@ -1,9 +1,14 @@
 package com.josealmeida.service;
 
+import com.josealmeida.dto.InventoryResponse;
 import com.josealmeida.repository.InventoryRepository;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +17,13 @@ public class InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).toList();
     }
 }
