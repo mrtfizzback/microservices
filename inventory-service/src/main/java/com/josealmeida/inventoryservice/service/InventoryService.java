0a8c95c9ee21cefc2ecb3fd92dabe124c1844fff
jose_Almeida <josepedrosmith@msn.com>
2024-02-11 01:15:45 +0000

    zipkin tracing working

--- inventory-service/src/main/java/com/josealmeida/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/josealmeida/inventoryservice/service/InventoryService.java
@@ -24,7 +24,7 @@ public class InventoryService {
     @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> skuCode) {
         log.info("Wait started");
-        Thread.sleep(10000);
+        Thread.sleep(50);
         log.info("Wait Ended");
         return inventoryRepository.findBySkuCodeIn(skuCode).stream()
                 .map(this::mapInventoryAndThrowErrorIfBadluck)
